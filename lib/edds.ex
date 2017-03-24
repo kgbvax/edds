972af7d5e4904a1b04899ae36296dab7138d0f68
Ingomar Otter <ingomar.otter@valtech.de>
2017-03-24 10:46:41 +0100

    initial

---
+++ lib/edds.ex
@@ -0,0 +1,18 @@
+defmodule Edds do
+  @moduledoc """
+  Documentation for Edds.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Edds.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/edds_test.exs
@@ -0,0 +1,8 @@
+defmodule EddsTest do
+  use ExUnit.Case
+  doctest Edds
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
